427208ceaa64e253731dbc9879e6346ad7a50138
Chetan417 <chetanprajapati7642@gmail.com>
2025-04-16 08:48:44 +0530

    first task

---
+++ task1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+print("this number addition is : ",num1 + num2)
+print("this number Subtraction is : ",num1 - num2)
+print("this number Multiplication is : ", num1 * num2)
+print("this number Division is : ", num1 / num2)
---
+++ task2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+
+print(f"hello, {first_name} {last_name}! welcome to the new Journy of python programing...")
