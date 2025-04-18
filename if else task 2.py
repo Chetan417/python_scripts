590597278dd0b224583920958b81d1f064f6f5ca
Chetan417 <chetanprajapati7642@gmail.com>
2025-04-18 10:26:16 +0530

    if...else statment task
    this is the if...else task .....

---
+++ if else task 2.py
@@ -0,0 +1,14 @@
+total = 0
+
+for i in range(1,51):
+    total += i
+
+print(f"this totoal is 1 to 50 number's sum total is : {total}")
+
+ # diffrent type of the task....
+
+n = int(input("Enter the number up to which you want to find the sum: "))
+total = 0
+for i in range(1,n+1):
+    total += i
+print(total)
---
+++ if...else task.py
@@ -0,0 +1,11 @@
+# if...else task 1 enter the number and get the output this number is even and odd......
+
+num = int(input("Enter a number : "))
+
+if num % 2 == 0 :
+    print(f"this {num} number is even")
+else   :
+    print(f"this {num} number is odd")
+
+
+
